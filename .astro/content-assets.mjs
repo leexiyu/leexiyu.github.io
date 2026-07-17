c2b2f2ab058624eeaef71e76bdec5a0038e68bd3
leexy <leexiyu@qq.com>
2026-07-17 09:52:14 +0800

    fix bug

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_Z1xqr5J from "../../assets/blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md";
+export default new Map([["../../assets/blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md", __ASTRO_IMAGE_IMPORT_Z1xqr5J]]);
+		
